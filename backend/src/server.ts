9b91d3d42cf260e667951bb6f3c907a00511e1d3
Hoppa <okwechimeosayeme@gmail.com>
2025-06-03 16:17:03 +0100

    demo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
-  res.send('API is Running 🚀');
+  res.send('API is still Running 🚀');
 });
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I have changed the code.
+          I have changed the code again.
         </p>
         <a
           className="App-link"
